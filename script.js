546a82a628b84772d0c5865e86504f122d4b6663
ahteshamsalamatansari <rdxahteshamsalamat@outlook.com>
2024-12-03 22:07:31 +0530

    3d Animationd done

---
+++ script.js
@@ -0,0 +1,110 @@
+function locomotiveanimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+const hero = document.querySelectorAll("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1")
+var counter = document.querySelector("#line1-part1 h5")
+var grow = 0;
+var tl = gsap.timeline();
+var page1 = document.querySelector("#page1")
+
+
+function loadingAnimation(){
+    tl.from(".line h1", {
+        y: 150,
+        stagger: 0.2,
+        duration: 0.6,
+        delay: 0.2
+    })
+    tl.from("#line1-part1", {
+        opacity: 0,
+        onStart: function () {
+            setInterval(function () {
+                if (grow < 100) {
+                    counter.innerHTML = grow++;
+                } else {
+                    counter.innerHTML = grow;
+                }
+            }, 33)
+        }
+    })
+    tl.to('.line h2',{
+        animationName:"NowAnimation",
+        opacity:1
+    })
+    tl.to("#loader", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 4
+    })
+    
+    tl.from(page1,{
+        delay:0.2,
+        y:1600,
+        ease:Power4,
+        duration:0.6
+    })
+    tl.to("#loader",{
+        display:"none"
+    })
+    tl.from("#nav",{
+        opacity:0
+    })
+    tl.from(hero,{
+        y:140,
+        stagger:0.2
+    })
+    tl.from("#hero1, #page2",{
+        opacity:0
+    },"-=1.2");
+}
+loadingAnimation();
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    Shery.makeMagnet("#nav-pt2 h4")
+}
+function sheryAnimation() {
+    Shery.imageEffect(".image-div", {
+      style: 5,
+      gooey: true,
+      debug:true,
+      config:{"a":{"value":2,"range":[0,30]},"b":{"value":-1,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272695760684946},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.23,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":1.53,"range":[0,10]},"metaball":{"value":0.33,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.01,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
+    });
+  }
+// cursorAnimation();
+locomotiveanimation();
+sheryAnimation();
